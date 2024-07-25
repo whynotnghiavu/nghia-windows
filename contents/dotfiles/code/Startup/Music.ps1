787f9059335f9df44ab1bd55816fa9d225b1586f
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-07-25 12:24:48 +0700

    update Music.ps1

--- contents/dotfiles/code/Startup/Music.ps1
+++ contents/dotfiles/code/Startup/Music.ps1
@@ -1,37 +1,4 @@
-# Create temp folder in startup
-Write-Host "Create temp folder in startup"
-
-$startupPath = [System.IO.Path]::Combine($env:APPDATA, "Microsoft\Windows\Start Menu\Programs\Startup")
-$tempFolderPath = [System.IO.Path]::Combine($startupPath, "temp")
-Write-Host "startupPath: $startupPath"
-Write-Host "tempFolderPath: $tempFolderPath"
-
-if (-Not (Test-Path -Path $tempFolderPath)) {
-    New-Item -ItemType Directory -Path $tempFolderPath -Force
-    Write-Output "The folder 'temp' has been created at: $tempFolderPath"
-} else {
-    Write-Output "The folder 'temp' already exists at: $tempFolderPath"
-}
-
-# Create Startup shortcut on Desktop
-Write-Host "Create Startup shortcut on Desktop"
-
-$startupPath = [System.IO.Path]::Combine($env:APPDATA, "Microsoft\Windows\Start Menu\Programs\Startup")
-Write-Host "startupPath: $startupPath"
-$desktopPath = [System.IO.Path]::Combine($env:USERPROFILE, "Desktop")
-Write-Host "desktopPath: $desktopPath"
-$desktopShortcutPath = [System.IO.Path]::Combine($desktopPath, "Startup.lnk")
-Write-Host "desktopShortcutPath: $desktopShortcutPath"
-
-$wshell = New-Object -ComObject WScript.Shell
-$shortcut = $wshell.CreateShortcut($desktopShortcutPath)
-$shortcut.TargetPath = $startupPath
-$shortcut.WorkingDirectory = $startupPath
-$shortcut.WindowStyle = 1
-$shortcut.Description = "Shortcut to Startup Folder"
-$shortcut.IconLocation = "shell32.dll, 34"
-$shortcut.Save()
-
+ 
 # Create Nghia folder in Downloads
 Write-Host "Create Nghia folder in Downloads"
 
@@ -59,6 +26,19 @@ if (-Not (Test-Path -Path $musicFolderPath)) {
 } else {
     Write-Output "The folder 'Music' already exists at: $musicFolderPath"
 }
+
+# Create temp folder in Music
+
+
+
+
+
+
+
+
+
+
+
 
 # Create Music shortcut on Desktop
 Write-Host "Create Music shortcut on Desktop"
