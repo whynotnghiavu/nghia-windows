6719a7686d27f14718c6bfa254bef664f18ec4f0
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-07-25 12:23:35 +0700

    update Music.ps1

--- contents/dotfiles/code/Startup/Music.ps1
+++ contents/dotfiles/code/Startup/Music.ps1
@@ -1,4 +1,4 @@
-#! Tạo thư mục temp trong startup
+# Create temp folder in startup
 Write-Host "Create temp folder in startup"
 
 $startupPath = [System.IO.Path]::Combine($env:APPDATA, "Microsoft\Windows\Start Menu\Programs\Startup")
@@ -13,7 +13,7 @@ if (-Not (Test-Path -Path $tempFolderPath)) {
     Write-Output "The folder 'temp' already exists at: $tempFolderPath"
 }
 
-#! Tạo shortcut của Startup trên Desktop
+# Create Startup shortcut on Desktop
 Write-Host "Create Startup shortcut on Desktop"
 
 $startupPath = [System.IO.Path]::Combine($env:APPDATA, "Microsoft\Windows\Start Menu\Programs\Startup")
@@ -32,10 +32,44 @@ $shortcut.Description = "Shortcut to Startup Folder"
 $shortcut.IconLocation = "shell32.dll, 34"
 $shortcut.Save()
 
+# Create Nghia folder in Downloads
+Write-Host "Create Nghia folder in Downloads"
 
+$downloadsPath = [System.IO.Path]::Combine($env:USERPROFILE, "Downloads")
+$nghiaFolderPath = [System.IO.Path]::Combine($downloadsPath, "Nghia")
+Write-Host "downloadsPath: $downloadsPath"
+Write-Host "nghiaFolderPath: $nghiaFolderPath"
 
+if (-Not (Test-Path -Path $nghiaFolderPath)) {
+    New-Item -ItemType Directory -Path $nghiaFolderPath -Force
+    Write-Output "The folder 'Nghia' has been created at: $nghiaFolderPath"
+} else {
+    Write-Output "The folder 'Nghia' already exists at: $nghiaFolderPath"
+}
 
-#! Tạo thư mục Nghia trong Downloads
-#! Tạo thư mục Music trong Nghia
-#! Tạo shortcut của Music trên Desktop
+# Create Music folder in Nghia
+Write-Host "Create Music folder in Nghia"
 
+$musicFolderPath = [System.IO.Path]::Combine($nghiaFolderPath, "Music")
+Write-Host "musicFolderPath: $musicFolderPath"
+
+if (-Not (Test-Path -Path $musicFolderPath)) {
+    New-Item -ItemType Directory -Path $musicFolderPath -Force
+    Write-Output "The folder 'Music' has been created at: $musicFolderPath"
+} else {
+    Write-Output "The folder 'Music' already exists at: $musicFolderPath"
+}
+
+# Create Music shortcut on Desktop
+Write-Host "Create Music shortcut on Desktop"
+
+$musicShortcutPath = [System.IO.Path]::Combine($desktopPath, "Music.lnk")
+Write-Host "musicShortcutPath: $musicShortcutPath"
+
+$shortcut = $wshell.CreateShortcut($musicShortcutPath)
+$shortcut.TargetPath = $musicFolderPath
+$shortcut.WorkingDirectory = $musicFolderPath
+$shortcut.WindowStyle = 1
+$shortcut.Description = "Shortcut to Music Folder"
+$shortcut.IconLocation = "shell32.dll, 239"
+$shortcut.Save()
